9f0764acf7268698c506d415c54bbe05201d4eeb
kidum-netizen <frankkibet96@gmail.com>
2024-04-09 03:16:26 -0700

    python exceptions

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,18 +1,47 @@
-#include <Python.h>
-#include <stdio.h>
-/**
- * print_python_float - gives data of the PyFloatObject
- * @p: the PyObject
+/*
+ * File: 103-python.c
+ * Auth: Type Your Name Here
  */
-void print_python_float(PyObject *p)
+
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
+void print_python_list(PyObject *p)
 {
-	double value = 0;
-	char *string = NULL;
+        Py_ssize_t size, alloc, i;
+        const char *type;
+        PyListObject *list = (PyListObject *)p;
+        PyVarObject *var = (PyVarObject *)p;
 
-	fflush(stdout);
-	printf("[.] float object info\n");
+        size = var->ob_size;
+        alloc = list->allocated;
 
-	if (!PyFloat_CheckExact(p))
-	{
-		printf("  [ERROR] Invalid Float Object\n");
-		return;
+        fflush(stdout);
+
+        printf("[*] Python list info\n");
+        if (strcmp(p->ob_type->tp_name, "list") != 0)
+        {
+                printf("  [ERROR] Invalid List Object\n");
+                return;
+        }
+
+        printf("[*] Size of the Python List = %ld\n", size);
+        printf("[*] Allocated = %ld\n", alloc);
+
+        for (i = 0; i < size; i++)
+        {
+                type = list->ob_item[i]->ob_type->tp_name;
+                printf("Element %ld: %s\n", i, type);
+                if (strcmp(type, "bytes") == 0)
+                        print_python_bytes(list->ob_item[i]);
+                else if (strcmp(type, "float") == 0)
+                        print_python_float(list->ob_item[i]);
+        }
+}
