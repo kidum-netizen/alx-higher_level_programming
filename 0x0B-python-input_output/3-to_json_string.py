2918805917926a9b4f2196ad94639d39a44303c1
kidum-netizen <frankkibet96@gmail.com>
2024-04-17 05:33:07 -0700

    python input and output correction

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-Contains the "to_json_string" fundtion
-"""
-
+"""Defines a function converting string-to-JSON."""
 import json
 
 
 def to_json_string(my_obj):
-    """returns the JSON representation of an object (string)"""
+    """Return the representation of a string in JSON."""
     return json.dumps(my_obj)
