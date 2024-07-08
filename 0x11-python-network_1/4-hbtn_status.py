79bceed76b845c9baad1ac304ca250f34d9126ce
kidum-netizen <frankkibet96@gmail.com>
2024-07-08 06:34:35 -0700

    python network 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-""" Uses requests module to fetch URL status"""
 
-import requests
+"""A script that fetches https://alx-intranet.hbtn.io/status
+"""
 
 if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+    import requests
+
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    req = requests.get(url)
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
